d293937ce88056bd0f8e7f782b82a96fcde458b6
Toran Sahu <toran.sahu@yahoo.com>
2022-11-11 11:58:54 +0530

    AP

--- toran/maths/formulae.py
+++ toran/maths/formulae.py
@@ -5,9 +5,29 @@
 
 """Formulae."""
 
-__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
-__license__ = 'Distributed under terms of the MIT license'
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
 
 
-def ap(a: int, n: int, d: int):
+def ap(a: int, n: int, d: int) -> int:
+    """Arithmatic Progression.
+
+    :param a: First term
+    :param n: Number of terms
+    :param d: Common difference between the terms
+    :return: Nth term
+    """
+
     return a + (n - 1) * d
+
+
+def sum_of_ap(a: int, n: int, d: int):
+    """Sum of Arithmatic Progression sequence.
+
+    :param a: First term
+    :param n: Number of terms
+    :param d: Common difference between the terms
+    :return: Sum
+    """
+
+    return (n / 2) * (2 * a + (n - 1) * d)
